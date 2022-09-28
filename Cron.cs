16b80546419070e86c60fa15c47940fcbaa44ef8
Егор Брон <71507444+EgorBron@users.noreply.github.com>
2022-09-28 16:45:59 +0300

    crontab patterns parse (begin of impl)

--- Cron.cs
+++ Cron.cs
@@ -13,19 +13,22 @@ namespace DESCEnd {
         
     }
 
-    public class CronTimeSpan : IComparable, IComparable<CronTimeSpan>, IEquatable<CronTimeSpan>, ISpanFormattable, IFormattable {
-        public int CompareTo(object? obj) => throw new NotImplementedException();
-        public int CompareTo(CronTimeSpan other) => throw new NotImplementedException();
-        public bool Equals(CronTimeSpan? other) => throw new NotImplementedException();
-        public string ToString(string? format, IFormatProvider? formatProvider) => throw new NotImplementedException();
-        public bool TryFormat(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider? provider) => throw new NotImplementedException();
+    public struct CronTimeContainer {
+        public int Years { get; set; } = 0;
+        public int Months { get; set; } = 0;
+        public int Days { get; set; } = 0;
+        public int Hours { get; set; } = 0;
+        public int Minutes { get; set; } = 0;
+        public int Seconds { get; set; } = 0;
+        public int DayOfWeek { get; set; } = 0;
+        public CronTimeContainer() { }
     }
 
     /// <summary>
     /// Class that represents task for <see cref="Cron"/>
     /// </summary>
     public class CronTask {
-        public CronTask(CronTimeSpan timeSpan) { }
+        public CronTask(CronTimeContainer container) { }
 
         /// <summary>
         /// Create task with time in *nix crontab format. Search in Google what is this, if you don't know (e. g. "crontab format").
@@ -33,7 +36,44 @@ namespace DESCEnd {
         /// <param name="pattern">Cron schedule pattern. In (1 2 3 4 dow) 1 is minute, 2 is hour, 3 is day, 4 is month, dow is day of week.</param>
         /// <returns>Scheduled task</returns>
         public static CronTask FromCrontabPattern(string pattern) {
-            return new CronTask(new CronTimeSpan());
+            string[] elems = pattern.Split(' ');
+            // TODO: ranges
+            var pat = new CronTimeContainer { };
+            if (elems.Length > 5) throw new ArgumentOutOfRangeException("crontab pattern contains more than 5 elements");
+
+            int GetSymbolValue(string el) {
+                int output;
+                if (!int.TryParse(el, out output)) {
+                    if (el.StartsWith("*")) {
+                        output = -1;
+                    }
+                    if (el.Contains("/")) {
+                        var repeats = el.Split('/');
+                        output /= Convert.ToByte(repeats[2]);
+                    }
+                }
+                return output;
+            }
+
+            for (int i = 0; i < elems.Length; i++) {
+                switch (i) {
+                    case 0: // minutes
+                        pat.Minutes = GetSymbolValue(elems[i]);
+                        break;
+                    case 1: // hours
+                        pat.Hours = GetSymbolValue(elems[i]);
+                        break;
+                    case 2: // days
+                        pat.Days = GetSymbolValue(elems[i]);
+                        break;
+                    case 3: // months
+                        pat.Months = GetSymbolValue(elems[i]);
+                        break;
+                    case 4: // day of week
+                        break;
+                }
+            }
+            return new CronTask(pat);
         }
         /// <summary>
         /// Create task with time in systemd.timer onCalendar format. Search in Google what is this, if you don't know (e. g. "systemd oncalendar format").
@@ -41,7 +81,7 @@ namespace DESCEnd {
         /// <param name="pattern">Cron schedule pattern. In (dow yy-mm-dd HH:MM:SS) dow is day of week, yy is year, mm is month, dd is day, HH is hour, MM is minute, SS is second.</param>
         /// <returns>Scheduled task</returns>
         public static CronTask FromSystemdOnCalendarPattern(string pattern) {
-            return new CronTask(new CronTimeSpan());
+            return new CronTask(new CronTimeContainer { });
         }
     }
     /// <summary>
